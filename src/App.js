366106b31511c88dd1e6818c1183260568447267
Hatif Khuld -X (hkhuld - WIPRO LIMITED at Cisco) <hkhuld@cisco.com>
2024-02-09 10:53:01 +0530

    ver2.0.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 export default class App extends Component {
-  a = 'Johny'
   render() {
     return (
       <div>
-       <NavBar/>
-       <News/>
+        <Router>
+          <NavBar/>
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={100}/>}/>
+            <Route exact path="/business" element={<News key="business" pageSize={100} category={'business'}/>}/>
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={100} category={'entertainment'}/>}/>
+            <Route exact path="/health" element={<News key="health" pageSize={100} category={'health'}/>}/>
+            <Route exact path="/science" element={<News key="science" pageSize={100} category={'science'}/>}/>
+            <Route exact path="/sports" element={<News key="sports" pageSize={100} category={'sports'}/>} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={100} category={'technology'}/>} />
+          </Routes>
+        </Router>
+       
+       
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
+import newsMonk from "./newsMonk.png"
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-            <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
-            </a>
-            <span ><b>News Monk</b></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between">
+          <div className="container-fluid ">
+            <Link className="navbar-brand" to="/">
+            <img src={newsMonk} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
+            </Link>
+            <span><b style={{fontFamily: `'Protest Revolution', sans-serif`}}>News Monk</b></span>
             <button
               className="navbar-toggler"
               type="button"
@@ -21,12 +23,43 @@ export default class NavBar extends Component {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
+            <div className="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo02">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li className="nav-item text-center">
+                  <Link disabled className="nav-link active" aria-current="page" to="/">
+                    <b></b>
+                  </Link>
+                </li>
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/business">
+                    <b>Business</b>
+                  </Link>
+                </li>
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/entertainment">
+                    <b>Entertainment</b>
+                  </Link>
+                </li>
+                 
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/health">
+                    <b>Health</b>
+                  </Link>
+                </li>
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/science">
+                    <b>Science</b>
+                  </Link>
+                </li>
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/sports">
+                    <b>Sports</b>
+                  </Link>
+                </li>
+                <li className="nav-item text-center">
+                  <Link className="nav-link active" aria-current="page" to="/technology">
+                    <b>Technology</b>
+                  </Link>
                 </li>
               </ul>
               
